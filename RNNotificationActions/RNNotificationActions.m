6008e63b54133e2602e6b2703fb4f496ff5c6c7f
Varun Gupta <varungupta303@gmail.com>
2017-03-07 15:48:58 +0530

    update headers to support rn >= 0.40

--- RNNotificationActions/RNNotificationActions.m
+++ RNNotificationActions/RNNotificationActions.m
@@ -2,10 +2,10 @@
 #import "RNNotificationActions.h"
 #import "RNNotificationActionsManager.h"
 
-#import "RCTBridge.h"
-#import "RCTConvert.h"
-#import "RCTEventDispatcher.h"
-#import "RCTUtils.h"
+#import <React/RCTBridge.h>
+#import <React/RCTConvert.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTUtils.h>
 
 NSString *const RNNotificationActionReceived = @"NotificationActionReceived";
 
