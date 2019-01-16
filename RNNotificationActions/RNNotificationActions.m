213a29f1691ff718af7bb623f7bc1ddd2da4b647
Varun Gupta <varungupta303@gmail.com>
2019-01-16 18:52:49 +0530

    Fix main queue setup warning (#21)
    * Fix main queue setup warning
    
    * Fix the background thread error

--- RNNotificationActions/RNNotificationActions.m
+++ RNNotificationActions/RNNotificationActions.m
@@ -115,7 +115,9 @@ RCT_EXPORT_METHOD(updateCategories:(NSArray *)json)
     UIUserNotificationType types = settings.types;
     
     // Update the settings for these types
-    [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:types categories:[NSSet setWithArray:categories]]];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:types categories:[NSSet setWithArray:categories]]];
+    });
 }
 
 RCT_EXPORT_METHOD(callCompletionHandler)
@@ -167,6 +169,11 @@ RCT_EXPORT_METHOD(callCompletionHandler)
 {
     [_bridge.eventDispatcher sendAppEventWithName:@"notificationActionReceived"
                                              body:notification.userInfo];
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
 }
 
 @end
